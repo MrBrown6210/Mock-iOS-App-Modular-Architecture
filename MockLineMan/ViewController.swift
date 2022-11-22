091c8d65e45d1309c07e29a2dcab26cf273b5697
MrBrown6210 <bite6210@gmail.com>
2022-11-22 19:04:03 +0700

    Initial Commit

---
+++ MockLineMan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MockLineMan
+//
+//  Created by Ratchata Nuanchan on 22/11/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
